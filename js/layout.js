eb843f7fd2fde4c51232ed2e1ae54ef8fbeb5c46
Filip Wieland <filiplamparski@gmail.com>
2014-07-29 23:05:41 +0100

    A more Material app bar

---
+++ js/layout.js
@@ -0,0 +1,31 @@
+$(function(){
+  function _resizePortalTableHeader(){
+    $('#portalTable > header').width($('#portalTable').width())
+      .css('top', ($('.app-bar').height() - 10).toString().concat('px'));
+  }
+  $('#portalTable > header').on('affix.bs.affix', _resizePortalTableHeader);
+  $(window).on('resize', _resizePortalTableHeader);
+
+  function _resizePortalTableHeaderCells(){
+    console.log('_resizePortalTableHeaderCells()');
+    $('#portalTable > table tr:nth-child(1) > td').each(function(i){
+      if (i === 0) {
+        // This absolutely HAS to be (desired width) - 10 pixels.
+        // Some really DUMB feature in Bootstrap resets the widths
+        // and somehow adds +10px to the first one. No, I don't
+        // know what it is, but it sucks.
+        $('#portalTable > header > table tr').children().eq(i).width(54);
+      } else {
+        $('#portalTable > header > table tr').children().eq(i).width($(this).width());
+      }
+    });
+  }
+  $(window).on('resize', _resizePortalTableHeaderCells);
+  $('#portalTable').on('custom.update', _resizePortalTableHeaderCells);
+
+  $('#portalTable > header').affix({
+    offset: {
+      top: $('.app-bar').height()
+    }
+  });
+});
--- js/mockables.js
+++ js/mockables.js
@@ -15,11 +15,13 @@ $(function(){
   function offs(i) {
     return Math.ceil(80 * (i / 130)).toString() + "%";
   }
-  var table = '<table class="table table-condensed table-hover">';
-  table = table.concat('<tr><th style="width: 50px"></th><th>Title</th><th>Submitted</th><th>State</th><th style="width: 400px"></th></tr>');
+  var table = '<table class="table table-hover">';
   for(i = 0; i < 130; i++) {
-    table = table.concat('<tr' + cls(i) + '><td style="width: 50px"><img src="http://placehold.it/48x48"/></td><td>Portal Title Here</td><td>23rd of July 2014</td><td>Submitted</td><td><div class="progress"><div class="progress-bar" style="margin-left: ' + offs(i) + '; width: 20%"></div></div></tr>');
+    table = table.concat('<tr' + cls(i) + '><td><img src="http://placehold.it/48x48" class="img img-circle"/></td><td>Portal Title Here</td><td>23rd of July 2014</td><td class="hidden-xs">Submitted</td><td class="hidden-xs"><b>83</b> days</tr>');
   }
   table = table.concat('</table>');
-  $('#portalTable').html(table);
+  $(table).appendTo('#portalTable');
+  setTimeout(function(){
+    $('#portalTable').trigger('custom.update'); // needed for positioning
+  }, 200);
 });
